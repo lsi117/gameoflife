ac3fa4823d036ab84ea189bf7dc22412110f77a3
lauren inacio <lauren_f23@yahoo.com>
2018-01-02 16:28:34 -0500

    working on css and all components showing up

--- src/index.js
+++ src/index.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class Grid extends React.Component{
+  render(){
+    return(
+        <div>
+          <h1>Grid</h1>
+        </div>
+      )
+  }
+}
+
+class Main extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      generation: 0,
+    }
+  }
+  render (){
+    return(
+      <div>
+        <h1>Jelly Fish</h1>
+        <p>Created by <a href="http://laureninacio.com/" target="_blank">Lauren Inacio</a></p>
+        <Grid />
+        <h2>Generations: {this.state.generation}</h2>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'));
