cab875288014fe1634a90458964e7ab94360e327
lauren inacio <lauren_f23@yahoo.com>
2018-01-06 00:45:33 -0500

    created buttons and incorperated react bootstrap

--- src/index.js
+++ src/index.js
@@ -44,7 +44,39 @@ class Grid extends React.Component{
 class Buttons extends React.Component{
   render(){
     return(
-      )
+      <div className="center">
+      <ButtonToolbar>
+        <button className="btn btn-default" onClick={this.props.playButton}>
+        Play
+        </button>
+        <button className="btn btn-default" onClick={this.props.pauseButton}>
+        Pause
+        </button>
+        <button className="btn btn-default" onClick={this.props.clear}>
+        Clear
+        </button>
+        <button className="btn btn-default" onClick={this.props.slow}>
+        Slow
+        </button>
+        <button className="btn btn-default" onClick={this.props.fast}>
+        Fast
+        </button>
+        <button className="btn btn-default" onClick={this.props.seed}>
+        Seed
+        </button>
+      <DropdownButton
+        title= "Grid Size"
+        id="Size Menu"
+        onSelect={this.handleSelect}
+        >
+
+        <MenuItem eventKey="1">20x10</MenuItem>
+        <MenuItem eventKey="2">50x30</MenuItem>
+        <MenuItem eventKey="3">70x50</MenuItem>
+        </DropdownButton>
+      </ButtonToolbar>
+    </div>
+    )
   }
 }
 
